02c59aa3c957be59826fe5705191dd8629822e72
Divyasri199 <83349577+Divyasri199@users.noreply.github.com>
2021-11-23 12:18:14 +0530

    Update rancher.ps1

--- rancher.ps1
+++ rancher.ps1
@@ -49,9 +49,25 @@ $trainerUserPassword = "Password.!!1"
 WindowsServerCommon
 InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
 
-$WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("https://raw.githubusercontent.com/sumitmalik51/Rbac-Policy/main/my-azure.txt","C:\LabFiles\my-azure.txt")
+Function CreadFiles($AzureTenantID, $AzureSubscriptionID, $SPID, $SPSecretKey)
+{
+    
+    New-Item -ItemType directory -Path C:\LabFiles -force
+
+    Add-Content -Path "C:\LabFiles\my-azure.txt" -Value "AzureTenantID= $AzureTenantID" -PassThru
+    Add-Content -Path "C:\LabFiles\my-azure.txt" -Value "AzureSubscriptionID= $AzureSubscriptionID" -PassThru
+    Add-Content -Path "C:\LabFiles\my-azure.txt" -Value "Application(Client)ID= $SPID" -PassThru
+    Add-Content -Path "C:\LabFiles\my-azure.txt" -Value "SecretKey= $SPSecretKey" -PassThru
+
+    (Get-Content -Path "C:\LabFiles\my-azure.txt") | ForEach-Object {$_ -Replace "AzureTenantID", "$AzureTenantID"} | Set-Content -Path "C:\LabFiles\my-azure.txt"
+    (Get-Content -Path "C:\LabFiles\my-azure.txt") | ForEach-Object {$_ -Replace "AzureSubscriptionID", "$AzureSubscriptionID"} | Set-Content -Path "C:\LabFiles\my-azure.txt"
+    (Get-Content -Path "C:\LabFiles\my-azure.txt") | ForEach-Object {$_ -Replace "Application(Client)ID", "$SPID"} | Set-Content -Path "C:\LabFiles\my-azure.txt"
+    (Get-Content -Path "C:\LabFiles\my-azure.txt") | ForEach-Object {$_ -Replace "SecretKey", "$SPSecretKey"} | Set-Content -Path "C:\LabFiles\my-azure.txt"
+    
     Copy-Item "C:\LabFiles\my-azure.txt" -Destination "C:\Users\Public\Desktop"
+}
+
+CreadFiles $AzureTenantID $AzureSubscriptionID $SPID $SPSecretKey
     
     
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
